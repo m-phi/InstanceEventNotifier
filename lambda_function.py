eaea2acdcb88bfb3569b33eb9d2be2f5f2ba4fff
Ashish <aaggarwal@siterx.com>
2025-01-20 17:39:26 -0500

    update comment

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 
-#HELLO FROM GITHUB
+#HELLO FROM GITHUB 2
 # SES configuration
 SES_REGION = 'us-east-1'  # Change to your SES region
 SENDER_EMAIL = 'aaggarwal@siterx.com'
