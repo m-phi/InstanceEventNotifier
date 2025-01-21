984f87c3af3252cbe6154b432c13fbea8ac3fa01
Ashish <aaggarwal@siterx.com>
2025-01-21 01:14:53 -0500

    added account in email

--- lambda_function.py
+++ lambda_function.py
@@ -12,6 +12,12 @@ SENDER_EMAIL = 'aaggarwal@siterx.com'
 RECIPIENT_EMAIL = 'aaggarwal@siterx.com'
 SUBJECT = 'EC2 Instance State Change'
 
+# Account to environment mapping
+ACCOUNT_ENV_MAPPING = {
+    '471536230503': 'ITOP Sandbox',
+    '381492225276': 'Security'
+}
+
 def lambda_handler(event, context):
     logger.info("Received event: %s", json.dumps(event))
 
@@ -21,30 +27,37 @@ def lambda_handler(event, context):
     state = detail.get('state', 'Unknown state')
 
     # Extract user identity ARN and email
-    # user_principalId = event.get('userIdentity', {}).get('principalId', 'Unknown user')
     user_identity_arn = event.get('userIdentity', {}).get('arn', 'Unknown user')
     user_email = user_identity_arn.split('/')[-1] if user_identity_arn != 'Unknown user' else 'Unknown user'
 
-    logger.info("Instance ID: %s, State: %s, User ARN: %s, User Email: %s", instance_id, state, user_identity_arn, user_email)
+    # Extract account number and determine environment
+    account_id = event.get('account', 'Unknown account')
+    environment = ACCOUNT_ENV_MAPPING.get(account_id, 'Unknown environment')
+    
+    # Extract account number and determine environment
+    account_id = event.get('account', 'Unknown account')
+    environment = ACCOUNT_ENV_MAPPING.get(account_id, 'Unknown environment')
+
+    logger.info("Instance ID: %s, State: %s, User ARN: %s, User Email: %s", instance_id, state, user_identity_arn, user_email, environment, account_id)
 
         
     if state in ['terminated', 'stopped']:
         # Send an email notification
-        send_email(instance_id, state, user_email)
+        send_email(instance_id, state, user_email, environment)
     
     return {
         'statusCode': 200,
         'body': json.dumps('Success')
     }
 
-def send_email(instance_id, state, user_email):
+def send_email(instance_id, state, user_email, environment):
     client = boto3.client('ses', region_name=SES_REGION)
     
     body_text = f"The EC2 instance with ID {instance_id} has been {state} by {user_email}."
     body_html = f"""<html>
     <head></head>
     <body>
-      <h1>EC2 Instance State Change</h1>
+      <h1>EC2 Instance State Change for {environment} account</h1>
       <p>The EC2 instance with ID <b>{instance_id}</b> has been <b>{state}</b> by user <b>{user_email}</b>.</p>
     </body>
     </html>"""
