182c3616e0e49040e1ddce4d596671d722a162e6
Ashish <aaggarwal@siterx.com>
2025-01-20 22:18:29 -0500

    included all instant state running, stopped and terminated

--- lambda_function.py
+++ lambda_function.py
@@ -1,41 +1,51 @@
 import json
 import boto3
+import logging
+
+# Configure logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # SES configuration
 SES_REGION = 'us-east-1'  # Change to your SES region
 SENDER_EMAIL = 'aaggarwal@siterx.com'
 RECIPIENT_EMAIL = 'aaggarwal@siterx.com'
-SUBJECT = 'EC2 Instance Stopped'
+SUBJECT = 'EC2 Instance State Change'
 
 def lambda_handler(event, context):
+    logger.info("Received event: %s", json.dumps(event))
+
     # Extract instance details from the event
-    detail = event['detail']
-    instance_id = detail['instance-id']
-    state = detail['state']
+    detail = event.get['detail', {}]
+    instance_id = detail['instance-id', 'Unknown instance')]
+    state = detail['state', 'Unknown state']
 
     # Extract user identity ARN and email
-    user_principalId = event.get('userIdentity', {}).get('principalId', 'Unknown user')
-    # user_identity_arn = event.get('userIdentity', {}).get('arn', 'Unknown user')
-    user_email = user_principalId.split(':')[1] if user_principalId != 'Unknown user' else 'Unknown user'
+    # user_principalId = event.get('userIdentity', {}).get('principalId', 'Unknown user')
+    user_identity_arn = event.get('userIdentity', {}).get('arn', 'Unknown user')
+    user_email = user_identity_arn.split(':')[-1] if user_identity_arn != 'Unknown user' else 'Unknown user'
+
+    logger.info("Instance ID: %s, State: %s, User ARN: %s, User Email: %s", instance_id, state, user_identity_arn, user_email)
+
         
-    if state == 'stopped':
+    if state in ['running', 'terminated', 'stopped']:
         # Send an email notification
-        send_email(instance_id, user_email)
+        send_email(instance_id, state, user_email)
     
     return {
         'statusCode': 200,
         'body': json.dumps('Success')
     }
 
-def send_email(instance_id, user_email):
+def send_email(instance_id, state, user_email):
     client = boto3.client('ses', region_name=SES_REGION)
     
-    body_text = f"The EC2 instance with ID {instance_id} has been stopped by {user_email}."
+    body_text = f"The EC2 instance with ID {instance_id} has been {state} by {user_email}."
     body_html = f"""<html>
     <head></head>
     <body>
-      <h1>EC2 Instance Stopped</h1>
-      <p>The EC2 instance with ID <b>{instance_id}</b> has been stopped by {user_email}.</p>
+      <h1>EC2 Instance State Change</h1>
+      <p>The EC2 instance with ID <b>{instance_id}</b> has been <b>{state}</b> by user <b>{user_email}</b>.</p>
     </body>
     </html>"""
     
