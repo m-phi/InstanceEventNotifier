04c1d088cd3954f433b17530fde3528199619ca5
Ashish <aaggarwal@siterx.com>
2025-01-20 22:05:57 -0500

    updated user_identity_arn to user_principleID test 2

--- lambda_function.py
+++ lambda_function.py
@@ -16,7 +16,7 @@ def lambda_handler(event, context):
     # Extract user identity ARN and email
     user_principalId = event.get('userIdentity', {}).get('principalId', 'Unknown user')
     # user_identity_arn = event.get('userIdentity', {}).get('arn', 'Unknown user')
-    user_email = user_principalId.split(':')[-1] if user_principalId != 'Unknown user' else 'Unknown user'
+    user_email = user_principalId.split(':')[1] if user_principalId != 'Unknown user' else 'Unknown user'
         
     if state == 'stopped':
         # Send an email notification
